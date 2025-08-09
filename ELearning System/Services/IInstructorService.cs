5058496d30dfc6ee586cc06e7d19323a0da2db69
hamata <ahmed.adham.1543@gmail.com>
2025-08-09 19:22:26 +0300

    template added

---
+++ ELearning System/Services/IInstructorService.cs
@@ -0,0 +1,13 @@
+﻿using ELearning_System.Models;
+
+namespace ELearning_System.Services
+{
+    public interface IInstructorService
+    {
+        Task<IEnumerable<Instructor>> GetAllInstructorsAsync();
+        Task<Instructor> GetInstructorByIdAsync(int id);
+        Task AddInstructorAsync(Instructor record);
+        Task UpdateInstructorAsync(Instructor record);
+        Task DeleteInstructorAsync(int id);
+    }
+}
---
+++ ELearning System/Services/InstructorService.cs
@@ -0,0 +1,43 @@
+﻿using ELearning_System.Models;
+using ELearning_System.Repositories;
+
+namespace ELearning_System.Services
+{
+    public class InstructorService : IInstructorService
+    {
+        private readonly IBaseRepository<Instructor> instructorRepo;
+
+        public InstructorService(IBaseRepository<Instructor> repo)
+        {
+            instructorRepo = repo;
+        }
+
+        public async Task AddInstructorAsync(Instructor record)
+        {
+            await instructorRepo.InsertAsync(record);
+            await instructorRepo.SaveAsync();
+        }
+
+        public async Task DeleteInstructorAsync(int id)
+        {
+            await instructorRepo.DeleteAsync(id);
+            await instructorRepo.SaveAsync();
+        }
+
+        public async Task<IEnumerable<Instructor>> GetAllInstructorsAsync()
+        {
+            return await instructorRepo.GetAllAsync();
+        }
+
+        public async Task<Instructor> GetInstructorByIdAsync(int id)
+        {
+            return await instructorRepo.GetByIdAsync(id);
+        }
+
+        public async Task UpdateInstructorAsync(Instructor record)
+        {
+            instructorRepo.Update(record);
+            await instructorRepo.SaveAsync();
+        }
+    }
+}
